2dc67f8893302209d0f300854cf5b6a19a2b68f3
Alex Maldonado <alexandermaldonadosoto@gmail.com>
2022-05-26 14:13:35 -0600

    fix: arregle algunos errores que se daban y no aceptaba el break

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let time = 0;
 let timer = null;
 let timerBreak = null;
 let current = null;
-
+let statusApp = "stop";
 
 const bAdd = document.querySelector("#bAdd");
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
+const taskName = document.querySelector( `#time #taskName`);
 
 form.addEventListener( "submit", (e) => {
 
@@ -43,13 +44,89 @@ function renderTasks(){
   });
 
   const tasksContainer = document.querySelector("#tasks");
-  tasksContainer.innerHTML = html.join (``);
+  tasksContainer.innerHTML = html.join("");
 
-  const startButtons = document.querySelectorAll(.`task .start-button`);
-
-  startButtons.forEach(button => {
-    button.addEventListener()
+  const startButtons = document.querySelectorAll(".task .start-button");
+  startButtons.forEach((startButton) => {
+    startButton.addEventListener("click", () => {
+      if(!timer){
+        startButtonHandler(startButton.getAttribute("data-id"));
+        startButton.textContent = "In progress..."; 
+      }
+    });
   });
+
+
+  function startButtonHandler(id) {
+    time = 2;
+    current = id;
+    const taskIndex = tasks.findIndex((task) => task.id == id);
+
+    taskName.textContent = tasks[taskIndex].title;
+
+
+
+////set interval ejecuta la accion cada cierto tiempo y son mil por que maneja milisegundos
+  timer = setInterval (() => {
+    timeHandler(id);
+  }, 1000);
+  }
+
+  function timeHandler(id){
+    time--;
+    renderTime();
+
+
+
+    if(time == 0){
+      clearInterval(timer);
+      markCompleted(id);
+      renderTasks();
+      startBreak();
+    }
+  }
+
+  function markCompleted(id) {
+    const taskId = tasks.findIndex((task) => task.id === id);
+    tasks[taskId].completed = true;
+  }
+
+  function startBreak(){
+    time = 3;
+    taskName.textContent = "Break";
+    timerBreak = setInterval(() => {
+      timerBreakHandler();
+    }, 1000);
+  }
+
+  function timerBreakHandler(){
+    timer--;
+    renderTime();
+
+    if(time == 0){
+      clearInterval(timerBreak);
+      current = null;
+      taskName.textContent = ``;
+      renderTasks();
+    }
+  }
 }
+
+  function renderTime(){
+    const timeDiv = document.querySelector(`#time #value`);
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+
+    timeDiv.textContent = `${minutes < 10 ? "0": ""}${minutes}:${seconds < 10 ? "0" : ""} ${seconds}`;
+
+  }
+
+function markCompleted(id){
+  const taskIndex = tasks.findIndex((task) => task.id == id);
+  task[taskIndex].completed = true;
+}
+
+
 
 
